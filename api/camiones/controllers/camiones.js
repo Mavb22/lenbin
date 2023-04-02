18118f069b4200fe4200044f39289d186c98d0a1
Mavb22 <86745873+LeninAdrian21@users.noreply.github.com>
2023-04-01 18:03:49 -0600

    Se comento las lineas del archivo de camiones

--- api/camiones/controllers/camiones.js
+++ api/camiones/controllers/camiones.js
@@ -1,26 +1,24 @@
 'use strict';
-
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
-
 module.exports = {
-  async  busqueda(ctx){
-    const { num_serie, niv, placas } = ctx.query;
-    const collectionName = 'camiones';
-    // const regex = new RegExp(placas, 'i');
-    const busqueda = await strapi.query(collectionName).find();
-    const regex_numserie = new RegExp(num_serie);
-    const regex_niv = new RegExp(niv);
-    const regex_placas = new RegExp(placas)
-    if(num_serie || niv){
-      const camiones = busqueda.filter((camion) => {
-         return camion.placas.filter((placa) => {
-            return regex_placas.test(placa.placas);
-         })
-      });
-      console.log(camiones)
-    }
-  }
+  // async  busqueda(ctx){
+  //   const { num_serie, niv, placas } = ctx.query;
+  //   const collectionName = 'camiones';
+  //   // const regex = new RegExp(placas, 'i');
+  //   const busqueda = await strapi.query(collectionName).find();
+  //   const regex_numserie = new RegExp(num_serie);
+  //   const regex_niv = new RegExp(niv);
+  //   const regex_placas = new RegExp(placas)
+  //   if(num_serie || niv){
+  //     const camiones = busqueda.filter((camion) => {
+  //        return camion.placas.filter((placa) => {
+  //           return regex_placas.test(placa.placas);
+  //        })
+  //     });
+  //     console.log(camiones)
+  //   }
+  // }
 };
