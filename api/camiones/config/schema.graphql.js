376b19325183eeba54046c87b6d2450eb6d72845
Geovanni Tamayo <geoipn@hotmail.com>
2023-04-01 21:34:03 -0600

    Creacion de la paginacion de camiones con error en totalcout

---
+++ api/camiones/config/schema.graphql.js
@@ -0,0 +1,115 @@
+// type PageInfoc{
+//     startCursor: ID
+//     endCursor: ID
+//     hasNextPage: Boolean!
+//     hasPreviousPage: Boolean!
+// }
+module.exports = {
+    definition: `
+    type CamionEdge{
+        node: Camiones
+        cursor: ID!
+    }
+    type CamionConnection{
+        toltalCount: Int!
+        edges: [CamionEdge!]!
+        pageInfo: PageInfo!
+    }
+    
+    
+    `,
+    query:`
+     paginationCamiones(
+        start: Int,
+        limit: Int,
+        placa: String,
+        estado: String,
+        placa_activa: Boolean,
+        num_serie: String,
+        niv: String,
+        fecha_inicio: Date,
+        fecha_fin: Date,
+        destino: String,
+        conductor: String,
+        gastos: String
+     ): CamionConnection
+    `,
+    resolver: {
+        Query: {
+            paginationCamiones:
+              async (obj, {start,limit,placa,estado,placa_activa,num_serie,niv,fecha_inicio,fecha_fin,destino,conductor,gastos}) => {
+                const startIndex = parseInt(start,10)>=0 ? parseInt(start,10) :0;
+                const query = {}
+                if(placa){
+                    const regex = new RegExp(placa, 'i')
+                    query['placas'] = {
+                      $elemMatch:{
+                        placa:{
+                          $regex: regex
+                        }
+                      }
+                    }
+                  }
+                if(estado){
+                    const regex = new RegExp(estado, 'i')
+                    query['placas'] = {
+                      $elemMatch:{
+                        estado:{
+                          $regex: regex
+                        }
+                      }
+                    }
+                  }
+                if(placa_activa){
+                    const regex = new RegExp(placa_activa, 'i')
+                    query['placas'] = {
+                      $elemMatch:{
+                        activa:{
+                          $regex: regex
+                        }
+                      }
+                    }
+                }
+                if(num_serie){
+                    const regex = new RegExp(num_serie, 'i');
+                    query.num_serie = { $regex: regex };
+                }
+                if(niv){
+                    const regex = new RegExp(niv, 'i');
+                    query.niv = { $regex: regex };
+                } 
+                if(destino){
+                    const regex = new RegExp(conductor, 'i'); 
+                    query["rutas.destino"] = { $regex: regex };
+                }
+                if(conductor){
+                    const regex = new RegExp(conductor, 'i'); 
+                    query["usuario.nombre"] = { $regex: regex };
+                }
+                if(gastos){
+                    const regex = new RegExp(gastos, 'i');
+                    query["gastos.categoria"] = { $regex: regex };
+                }
+                const camiones = await strapi.query('camiones').find(query);
+                const edges = camiones
+                .slice(startIndex, startIndex + parseInt(limit))
+                .map((abono) => ({ node: abono, cursor: abono.id }));
+              const pageInfo = {
+                startCursor: edges.length > 0 ? edges[0].cursor : null,
+                endCursor: edges.length > 0 ? edges[edges.length - 1].cursor : null,
+                hasNextPage:  startIndex + parseInt(limit) < camiones.length,
+                hasPreviousPage: startIndex > 0,
+              };
+              return {
+                totalCount: parseInt(camiones.length),
+                edges,
+                pageInfo,
+              };
+
+              }
+        }
+    }
+    
+    
+    
+}
--- api/camiones/controllers/camiones.js
+++ api/camiones/controllers/camiones.js
@@ -4,21 +4,21 @@
  * to customize this controller
  */
 module.exports = {
-  // async  busqueda(ctx){
-  //   const { num_serie, niv, placas } = ctx.query;
-  //   const collectionName = 'camiones';
-  //   // const regex = new RegExp(placas, 'i');
-  //   const busqueda = await strapi.query(collectionName).find();
-  //   const regex_numserie = new RegExp(num_serie);
-  //   const regex_niv = new RegExp(niv);
-  //   const regex_placas = new RegExp(placas)
-  //   if(num_serie || niv){
-  //     const camiones = busqueda.filter((camion) => {
-  //        return camion.placas.filter((placa) => {
-  //           return regex_placas.test(placa.placas);
-  //        })
-  //     });
-  //     console.log(camiones)
-  //   }
-  // }
+  async  busqueda(ctx){
+    const { num_serie, niv, placas } = ctx.query;
+    const collectionName = 'camiones';
+    // const regex = new RegExp(placas, 'i');
+    const busqueda = await strapi.query(collectionName).find();
+    const regex_numserie = new RegExp(num_serie);
+    const regex_niv = new RegExp(niv);
+    const regex_placas = new RegExp(placas)
+    if(num_serie || niv){
+      const camiones = busqueda.filter((camion) => {
+         return camion.placas.filter((placa) => {
+            return regex_placas.test(placa.placas);
+         })
+      });
+      console.log(camiones)
+    }
+  }
 };
